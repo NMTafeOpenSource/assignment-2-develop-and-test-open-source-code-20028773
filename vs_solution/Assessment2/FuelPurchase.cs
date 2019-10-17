cc1c77653d4cbd5f2868457ba2cfff6813882920
Koji <20028773@tafe.wa.edu.au>
2019-10-17 10:06:29 +0800

    FuelPurchase class created

---
+++ vs_solution/Assessment2/FuelPurchase.cs
@@ -0,0 +1,29 @@
+﻿namespace Assessment2
+{
+    public class FuelPurchase
+    {
+        private double _fuelEconomy;
+        private double _litres = 0;
+        private double _cost = 0;
+
+        public double getFuelEconomy()
+        {
+            return _fuelEconomy;
+        }
+
+        public double getFuel()
+        {
+            return _litres;
+        }
+
+        public void setFuelEconomy(double fuelEconomy)
+        {
+            _fuelEconomy = fuelEconomy;
+        }
+        public void purchaseFuel(double amount, double price)
+        {
+            _litres += amount;
+            _cost += price;
+        }
+    }
+}
