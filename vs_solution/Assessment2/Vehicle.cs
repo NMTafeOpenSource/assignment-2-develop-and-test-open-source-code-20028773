b28af4216eff6747c20b2f74ecf51191a9c9d262
Koji <20028773@tafe.wa.edu.au>
2019-10-17 10:33:27 +0800

    Vehicle class created

---
+++ vs_solution/Assessment2/Vehicle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment2
+{
+    public class Vehicle
+    {
+        private string _manufacturer;
+        private string _model;
+        private int _makeYear;
+        // TODO add Registration Number 
+        // TODO add variable for OdometerReading (in KM), 
+        // TODO add variable for TankCapacity (in litres)
+
+        private FuelPurchase fuelPurchase;
+
+        /**
+         * Class constructor specifying name of make (manufacturer), model and year
+         * of make.
+         * @param manufacturer
+         * @param model
+         * @param makeYear
+         */
+        public Vehicle(string manufacturer, string model, int makeYear)
+        {
+            _manufacturer = manufacturer;
+            _model = model;
+            _makeYear = makeYear;
+            fuelPurchase = new FuelPurchase();
+        }
+
+        // TODO Add missing getter and setter methods
+
+        /**
+         * Prints details for {@link Vehicle}
+         */
+        public string printDetails()
+        {
+            return "Vehicle: " + _makeYear + " " + _manufacturer + " " + _model;
+            // TODO Display additional information about this vehicle
+        }
+
+
+        // TODO Create an addKilometers method which takes a parameter for distance travelled 
+        // and adds it to the odometer reading. 
+
+        // adds fuel to the car
+        public void addFuel(double litres, double price)
+        {
+            fuelPurchase.purchaseFuel(litres, price);
+        }
+
+    }
+}
