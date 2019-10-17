ed228336eb518718463232b07943c3dc92c4bf3f
Koji <20028773@tafe.wa.edu.au>
2019-10-17 10:38:48 +0800

    Starter code added to VS solution

--- vs_solution/Assessment2/MainWindow.xaml.cs
+++ vs_solution/Assessment2/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Assessment2
 {
@@ -23,6 +11,14 @@ namespace Assessment2
         public MainWindow()
         {
             InitializeComponent();
+
+            Vehicle v = new Vehicle("Ford", "T812", 2014);
+
+            // Vehicle sample distance
+            v.addFuel(new Random().NextDouble() * 10, 1.3);
+
+            v.printDetails();
+            // System.out.println("\n\n");
         }
     }
 }
