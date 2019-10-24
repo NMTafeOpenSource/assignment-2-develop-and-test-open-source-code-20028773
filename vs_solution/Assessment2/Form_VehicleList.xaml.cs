dc7d4597655a19bb758853a1dab46afad32ef719
Koji <20028773@tafe.wa.edu.au>
2019-10-24 09:44:16 +0800

    List Vehicle Form Created

---
+++ vs_solution/Assessment2/Form_VehicleList.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+
+namespace Assessment2
+{
+    /// <summary>
+    /// Interaction logic for Form_VehicleList.xaml
+    /// </summary>
+    public partial class Form_VehicleList : Window
+    {
+        private Vehicle vehicle = new Vehicle();
+
+        public Form_VehicleList()
+        {
+            InitializeComponent();
+
+            UpdateList();
+        }
+
+        public void UpdateList(int nIndex = 0)
+        {
+            vehicle.LoadVehicles();
+            lvVehicleList.ItemsSource = vehicle.VehiclesList;
+            lvVehicleList.Items.Refresh();
+
+            //txtRecordNumberLabel.Text = string.Format("Record {0} of {1}", nIndex + 1, companyList.Count);
+        }
+
+        private void DeleteVehicleButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditVehicleButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
