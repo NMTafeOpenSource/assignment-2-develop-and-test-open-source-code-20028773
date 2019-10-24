69319183c7d6bf5e197696994e925c3f132ebcca
Koji <20028773@tafe.wa.edu.au>
2019-10-24 13:49:16 +0800

    Implementeded the code behind for the form

--- vs_solution/Assessment2/Form_Vehicle.xaml.cs
+++ vs_solution/Assessment2/Form_Vehicle.xaml.cs
@@ -7,9 +7,52 @@ namespace Assessment2
     /// </summary>
     public partial class Form_Vehicle : Window
     {
+        Vehicle vehicle = new Vehicle();
+
+        private int id = 0;
+
         public Form_Vehicle()
         {
             InitializeComponent();
+
+            txtManufacturer.Focus();
+        }
+
+        public Form_Vehicle(Vehicle updateVehicle)
+        {
+            InitializeComponent();
+
+            txtManufacturer.Focus();
+
+            id = updateVehicle.Id;
+
+            txtManufacturer.Text = updateVehicle.Manufacturer;
+            txtModel.Text = updateVehicle.Model;
+            txtYear.Text = updateVehicle.MakeYear.ToString();
+            txtRegistration.Text = updateVehicle.RegistrationNumber;
+            txtOdometer.Text = updateVehicle.OdometerReading.ToString();
+            txtTank.Text = updateVehicle.TankCapacity.ToString();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (id != 0)
+            {
+                vehicle.EditVehicle(Form_VehicleList.vehicleList, id, txtManufacturer.Text, txtModel.Text, int.Parse(txtYear.Text), txtRegistration.Text, double.Parse(txtOdometer.Text), double.Parse(txtTank.Text));
+            }
+            else
+            {
+                vehicle.AddVehicle(Form_VehicleList.vehicleList, txtManufacturer.Text, txtModel.Text, int.Parse(txtYear.Text), txtRegistration.Text, double.Parse(txtOdometer.Text), double.Parse(txtTank.Text));
+            }
+
+            MessageBox.Show("Vehicle " + (id != 0 ? "Edited" : "Created") + " Successfully!", "Vehicle", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            this.Close();
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
