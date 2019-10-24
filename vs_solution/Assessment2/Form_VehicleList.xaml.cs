e0fbbaa58f59c62a9a5aa480ef6d83ab8ebc7b91
Koji <20028773@tafe.wa.edu.au>
2019-10-24 13:48:10 +0800

    Implemented the Add Vehicle button, Edit, delete and list double click methods

--- vs_solution/Assessment2/Form_VehicleList.xaml.cs
+++ vs_solution/Assessment2/Form_VehicleList.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Assessment2
 {
@@ -7,7 +9,8 @@ namespace Assessment2
     /// </summary>
     public partial class Form_VehicleList : Window
     {
-        private Vehicle vehicle = new Vehicle();
+        //private Vehicle vehicle = new Vehicle();
+        public static List<Vehicle> vehicleList;
 
         public Form_VehicleList()
         {
@@ -18,21 +21,43 @@ namespace Assessment2
 
         public void UpdateList(int nIndex = 0)
         {
-            vehicle.LoadVehicles();
-            lvVehicleList.ItemsSource = vehicle.VehiclesList;
+            vehicleList = Vehicle.LoadVehicles();
+            lvVehicleList.ItemsSource = vehicleList;
             lvVehicleList.Items.Refresh();
-
-            //txtRecordNumberLabel.Text = string.Format("Record {0} of {1}", nIndex + 1, companyList.Count);
         }
 
         private void DeleteVehicleButton_Click(object sender, RoutedEventArgs e)
         {
-
+            Button button = sender as Button;
+            Vehicle vehicleItem = button.DataContext as Vehicle;
+            Vehicle.DeleteVehicle(vehicleList, vehicleItem);
+            UpdateList();
         }
 
         private void EditVehicleButton_Click(object sender, RoutedEventArgs e)
         {
+            Button button = sender as Button;
+            Vehicle vehicleItem = button.DataContext as Vehicle;
+            Form_Vehicle form_Vehicle = new Form_Vehicle(vehicleItem);
+            form_Vehicle.ShowDialog();
+            UpdateList();
+        }
 
+        private void BtnAddVehicle_Click(object sender, RoutedEventArgs e)
+        {
+            Form_Vehicle form_Vehicle = new Form_Vehicle();
+            form_Vehicle.ShowDialog();
+            UpdateList();
+        }
+
+        private void LvVehicleList_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (lvVehicleList.SelectedItem != null)
+            {
+                Form_Vehicle form_Vehicle = new Form_Vehicle((Vehicle)lvVehicleList.SelectedItem);
+                form_Vehicle.ShowDialog();
+                UpdateList();
+            }
         }
     }
 }
