803f60442cc940a4e62d2590eb3428f768da7d5d
Koji <20028773@tafe.wa.edu.au>
2019-10-24 09:48:03 +0800

    List Vehicle Form Created v2

--- vs_solution/Assessment2/MainWindow.xaml.cs
+++ vs_solution/Assessment2/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 
 namespace Assessment2
@@ -8,17 +9,24 @@ namespace Assessment2
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        List<Vehicle> vehicleList = new List<Vehicle>();
+
         public MainWindow()
         {
             InitializeComponent();
 
-            Vehicle v = new Vehicle("Ford", "T812", 2014, "1CES418", 153132.2, 45.5);
+            //Vehicle v = new Vehicle("Ford", "T812", 2014, "1CES418", 153132.2, 45.5);
 
             // Vehicle sample distance
-            v.addFuel(new Random().NextDouble() * 10, 1.3);
+            //v.addFuel(new Random().NextDouble() * 10, 1.3);
 
-            v.printDetails();
+            //v.printDetails();
             // System.out.println("\n\n");
+            //new Vehicle().AddVehicle("Ford", "T812", 2014, "1CES418", 153132.2, 45.5);
+
+            Form_VehicleList form_VehicleList = new Form_VehicleList();
+            form_VehicleList.ShowDialog();
         }
     }
 }
