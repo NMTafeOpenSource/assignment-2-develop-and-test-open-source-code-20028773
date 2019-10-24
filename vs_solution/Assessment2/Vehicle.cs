9150facf0911ff8fbb55767d6e718afc8b16996d
Koji <20028773@tafe.wa.edu.au>
2019-10-24 09:46:02 +0800

    Vehicle class updated to implement the Json class

--- vs_solution/Assessment2/Vehicle.cs
+++ vs_solution/Assessment2/Vehicle.cs
@@ -1,24 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Assessment2
 {
     public class Vehicle
     {
+        private int _id = 0;
         private string _manufacturer;
         private string _model;
         private int _makeYear;
         private string _registrationNumber;
         private double _odometerReading;
         private double _tankCapacity;
-        //  add Registration Number 
-        //  add variable for OdometerReading (in KM), 
-        //  add variable for TankCapacity (in litres)
+
+        public int Id { get { return _id; } set { _id = value + 1; } }
+        public string Manufacturer { get { return _manufacturer; } set { _manufacturer = value; } }
+        public string Model { get { return _model; } set { _model = value; } }
+        public int MakeYear { get { return _makeYear; } set { _makeYear = value; } }
+        public string RegistrationNumber { get { return _registrationNumber; } set { _registrationNumber = value; } }
+        public double OdometerReading { get { return _odometerReading; } set { _odometerReading = value; } }
+        public double TankCapacity { get { return _tankCapacity; } set { _tankCapacity = value; } }
+
+        public List<Vehicle> VehiclesList { get { return _vehicles; } }
+
 
         private FuelPurchase fuelPurchase;
+
+        private List<Vehicle> _vehicles = new List<Vehicle>();
+
+        public Vehicle() { }
 
         /**
          * Class constructor specifying name of make (manufacturer), model and year
@@ -27,16 +36,28 @@ namespace Assessment2
          * @param model
          * @param makeYear
          */
-        public Vehicle(string manufacturer, string model, int makeYear, string registrationNumber, double odometerReading, double tankCapacity)
+        private Vehicle(int id, string manufacturer, string model, int makeYear, string registrationNumber, double odometerReading, double tankCapacity)
         {
-            _manufacturer = manufacturer;
-            _model = model;
-            _makeYear = makeYear;
-            _registrationNumber = registrationNumber;
-            _odometerReading = odometerReading;
-            _tankCapacity = tankCapacity;
+            Id = id;
+            Manufacturer = manufacturer;
+            Model = model;
+            MakeYear = makeYear;
+            RegistrationNumber = registrationNumber;
+            OdometerReading = odometerReading;
+            TankCapacity = tankCapacity;
 
             fuelPurchase = new FuelPurchase();
+        }
+
+        public void AddVehicle(string manufacturer, string model, int makeYear, string registrationNumber, double odometerReading, double tankCapacity)
+        {
+            _vehicles.Add(new Vehicle(_vehicles.Count, manufacturer, model, makeYear, registrationNumber, odometerReading, tankCapacity));
+            JsonData.SaveCompany(_vehicles);
+        }
+
+        public void LoadVehicles()
+        {
+            _vehicles = JsonData.LoadCompanies<Vehicle>();
         }
 
         /**
@@ -44,7 +65,7 @@ namespace Assessment2
          */
         public string printDetails()
         {
-            return string.Format("Vehicle: {0} {1} {2} {3} {4} {5}", _makeYear, _manufacturer, _model, _registrationNumber, _odometerReading, _tankCapacity);
+            return string.Format("Vehicle: {0} {1} {2} {3} {4} {5}", MakeYear, Manufacturer, Model, RegistrationNumber, OdometerReading, TankCapacity);
         }
 
 
@@ -52,9 +73,8 @@ namespace Assessment2
         // and adds it to the odometer reading. 
         public void addKilometers(double distance)
         {
-            _odometerReading += distance;
+            OdometerReading += distance;
         }
-
 
         // adds fuel to the car
         public void addFuel(double litres, double price)
