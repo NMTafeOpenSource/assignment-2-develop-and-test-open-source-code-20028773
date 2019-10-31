267304e770d714c6694eb92e5289f5b45e36f34e
Koji <20028773@tafe.wa.edu.au>
2019-10-31 15:27:07 +0800

    Finalize Rental Method Created and porperty CustomerId changed to CustomerName

--- vs_solution/Assessment2/Rental.cs
+++ vs_solution/Assessment2/Rental.cs
@@ -8,7 +8,7 @@ namespace Assessment2
     {
         public int Id { get; set; }
         public int vehicleId { get; set; }
-        public int customerId { get; set; }
+        public string customerName { get; set; }
         public type rentType { get; set; }
         public double startOdometer { get; set; }
         public double endOdometer { get; set; }
@@ -18,19 +18,36 @@ namespace Assessment2
         public double totalPrice { get; set; }
         public DateTime ModifiedDate { get; set; }
 
+        public string startDateToString { get { return startDate.ToString("dd/MM/yyyy"); } }
+
+        public string vehicleDescription
+        {
+            get
+            {
+                var sAux = "";
+
+                if (MainWindow.vehicleList != null)
+                {
+                    sAux = MainWindow.vehicleList.Where(x => x.Id == vehicleId).Select(f => f.Manufacturer + " - " + f.Model).FirstOrDefault();
+                }
+
+                return sAux;
+            }
+        }
+
         public enum type
         {
-            day,
-            km
+            Day,
+            KM
         }
 
         public Rental() { }
 
-        public Rental(int id,int vehicleId, int customerId, type rentType, double startOdometer, double endOdometer, DateTime startDate, DateTime endDate, string notes, double totalPrice)
+        public Rental(int id, int vehicleId, string customerName, type rentType, double startOdometer, double endOdometer, DateTime startDate, DateTime endDate, string notes, double totalPrice)
         {
             this.Id = id;
             this.vehicleId = vehicleId;
-            this.customerId = customerId;
+            this.customerName = customerName;
             this.rentType = rentType;
             this.startOdometer = startOdometer;
             this.endOdometer = endOdometer;
@@ -41,10 +58,31 @@ namespace Assessment2
             this.ModifiedDate = DateTime.Now;
         }
 
-        public void AddRental(List<Rental> rentalList, int vehicleId, int customerId, type rentType, double startOdometer, double endOdometer, DateTime startDate, DateTime endDate, string notes, double totalPrice)
+        public void AddRental(List<Rental> rentalList, int vehicleId, string customerName, type rentType, double startOdometer, double endOdometer, DateTime startDate, DateTime endDate, string notes, double totalPrice)
         {
-            rentalList.Add(new Rental((rentalList.Count > 0 ? rentalList.Last().Id + 1 : 1), vehicleId, customerId, rentType, startOdometer, endOdometer, startDate, endDate, notes, totalPrice));
+            rentalList.Add(new Rental((rentalList.Count > 0 ? rentalList.Last().Id + 1 : 1), vehicleId, customerName, rentType, startOdometer, endOdometer, startDate, endDate, notes, totalPrice));
             JsonData.Save(rentalList);
+        }
+
+        public void FinalizeRental(List<Rental> rentalList, int id, double endOdometer, DateTime endDate, string notes)
+        {
+            Rental r = rentalList.Where(x => x.Id == id).FirstOrDefault();
+
+            var totalPrice = r.rentType == type.KM
+                            ? endOdometer - r.startOdometer
+                            : ((endDate - r.startDate).Days > 0 ? (endDate - r.startDate).Days : 1) * 100;
+
+            r.endOdometer = endOdometer;
+            r.endDate = endDate;
+            r.notes = notes;
+            r.totalPrice = totalPrice;
+            r.ModifiedDate = DateTime.Now;
+
+            rentalList.ToArray().SetValue(r, 0);
+
+            JsonData.Save(rentalList);
+
+            new Vehicle().UpdateOdometer(MainWindow.vehicleList, r.vehicleId, endOdometer);
         }
 
         public static List<Rental> LoadRental()
