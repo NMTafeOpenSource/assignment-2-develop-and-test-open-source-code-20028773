8fc252743e781abe8c91cb83f2c11c3cbc1f3cf5
Marcelo Furukawa <marcelokfu@gmail.com>
2019-11-27 21:36:08 +0800

    Unit Tests Added

---
+++ vs_solution/UnitTestProject1/RentalClassTest.cs
@@ -0,0 +1,155 @@
+﻿using Assessment2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class RentalClassTest
+    {
+        Rental r;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            TestClass.initializeFiles();
+        }
+
+        [TestMethod]
+        public void addRental_Test()
+        {
+            var listCountBefore = Rental.rentalList.Count;
+
+            Rental.AddRental(TestClass.v.Id, "Koji Furukawa Test", Rental.type.Day, TestClass.v.OdometerReading, DateTime.Now, null, string.Empty);
+
+            var listCountAfter = Rental.rentalList.Count;
+
+            Assert.AreEqual(listCountBefore + 1, listCountAfter);
+        }
+
+        [TestMethod]
+        public void finalizeRental_Test()
+        {
+            Rental.AddRental(TestClass.v.Id, "Koji Furukawa Test", Rental.type.Day, TestClass.v.OdometerReading, DateTime.Now, null, string.Empty);
+
+            r = Rental.rentalList.Where(x => x.customerName.Contains("Koji Furukawa Test")).FirstOrDefault();
+
+            Rental.FinalizeRental(r.Id, 2000, DateTime.Now.AddDays(10), string.Empty);
+
+            r = Rental.rentalList.Where(x => x.customerName.Contains("Koji Furukawa Test")).FirstOrDefault();
+
+            Assert.AreEqual(r.endOdometer, 2000);
+            Assert.AreEqual(r.endDate.Value.ToString(), DateTime.Now.AddDays(10).ToString());
+        }
+
+        [TestMethod]
+        public void getTotalRevenue_PerDay_Test()
+        {
+            Rental.AddRental(TestClass.v.Id, "Koji Furukawa Test", Rental.type.Day, TestClass.v.OdometerReading, DateTime.Now, null, string.Empty);
+
+            r = Rental.rentalList.Where(x => x.customerName.Contains("Koji Furukawa Test")).FirstOrDefault();
+
+            Rental.FinalizeRental(r.Id, 2000, DateTime.Now.AddDays(10), string.Empty);
+
+            double result = Rental.GetTotalRevenue(TestClass.v.Id);
+
+            Assert.AreEqual(result, 1000);
+        }
+
+        [TestMethod]
+        public void getTotalRevenue_PerKM_Test()
+        {
+            Rental.AddRental(TestClass.v.Id, "Koji Furukawa Test", Rental.type.KM, TestClass.v.OdometerReading, DateTime.Now, null, string.Empty);
+
+            r = Rental.rentalList.Where(x => x.customerName.Contains("Koji Furukawa Test")).FirstOrDefault();
+
+            Rental.FinalizeRental(r.Id, 2000, DateTime.Now.AddDays(10), string.Empty);
+
+            double result = Rental.GetTotalRevenue(TestClass.v.Id);
+
+            Assert.AreEqual(result, 1000);
+        }
+
+        [TestMethod]
+        public void getRentalList_Test()
+        {
+            Rental.AddRental(TestClass.v.Id, "Koji Furukawa Test", Rental.type.KM, TestClass.v.OdometerReading, DateTime.Now, null, string.Empty);
+
+            r = Rental.rentalList.Where(x => x.customerName.Contains("Koji Furukawa Test")).FirstOrDefault();
+
+            Rental.FinalizeRental(r.Id, 2000, DateTime.Now.AddDays(10), string.Empty);
+
+            List<Rental> result = Rental.getRentalList();
+
+            Assert.AreEqual(result.Count, 0);
+        }
+
+        [TestMethod]
+        public void getRentalList_Test2()
+        {
+            Rental.AddRental(TestClass.v.Id, "Koji Furukawa Test", Rental.type.KM, TestClass.v.OdometerReading, DateTime.Now, null, string.Empty);
+
+            r = Rental.rentalList.Where(x => x.customerName.Contains("Koji Furukawa Test")).FirstOrDefault();
+
+            Rental.FinalizeRental(r.Id, 2000, DateTime.Now.AddDays(10), string.Empty);
+
+            List<Rental> result = Rental.getRentalList("", true);
+
+            Assert.AreEqual(result.Count, 1);
+        }
+
+        [TestMethod]
+        public void getRentalList_Test3()
+        {
+            Rental.AddRental(TestClass.v.Id, "Koji Furukawa Test", Rental.type.KM, TestClass.v.OdometerReading, DateTime.Now, null, string.Empty);
+
+            r = Rental.rentalList.Where(x => x.customerName.Contains("Koji Furukawa Test")).FirstOrDefault();
+
+            Rental.FinalizeRental(r.Id, 2000, DateTime.Now.AddDays(10), string.Empty);
+
+            List<Rental> result = Rental.getRentalList("Koji");
+
+            Assert.AreEqual(result.Count, 0);
+        }
+
+        [TestMethod]
+        public void getRentalList_Test4()
+        {
+            Rental.AddRental(TestClass.v.Id, "Koji Furukawa Test", Rental.type.KM, TestClass.v.OdometerReading, DateTime.Now, null, string.Empty);
+
+            r = Rental.rentalList.Where(x => x.customerName.Contains("Koji Furukawa Test")).FirstOrDefault();
+
+            Rental.FinalizeRental(r.Id, 2000, DateTime.Now.AddDays(10), string.Empty);
+
+            List<Rental> result = Rental.getRentalList("Koji", true);
+
+            Assert.AreEqual(result.Count, 1);
+        }
+
+        [TestMethod]
+        public void GetAvailableVehicles_Test()
+        {
+            Rental.AddRental(TestClass.v.Id, "Koji Furukawa Test", Rental.type.KM, TestClass.v.OdometerReading, DateTime.Now, null, string.Empty);
+
+            List<Vehicle> result = Rental.GetAvailableVehicles();
+
+            Assert.AreEqual(result.Count, 0);
+        }
+
+        [TestMethod]
+        public void GetAvailableVehicles_Test2()
+        {
+            Rental.AddRental(TestClass.v.Id, "Koji Furukawa Test", Rental.type.KM, TestClass.v.OdometerReading, DateTime.Now, null, string.Empty);
+
+            r = Rental.rentalList.Where(x => x.customerName.Contains("Koji Furukawa Test")).FirstOrDefault();
+
+            Rental.FinalizeRental(r.Id, 2000, DateTime.Now.AddDays(10), string.Empty);
+
+            List<Vehicle> result = Rental.GetAvailableVehicles();
+
+            Assert.AreEqual(result.Count, 1);
+        }
+    }
+}
---
+++ vs_solution/UnitTestProject1/VehicleClassTest.cs
@@ -0,0 +1,65 @@
+﻿using Assessment2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class VehicleCLassTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            TestClass.initializeFiles();
+        }
+
+        [TestMethod]
+        public void addVehicle_Test()
+        {
+            var listCountBefore = Vehicle.vehicleList.Count;
+
+            Vehicle.AddVehicle("TestMake", "testmodel", 2019, "1DAS123", 1000, 50);
+
+            var listCountAfter = Vehicle.vehicleList.Count;
+
+            Assert.AreEqual(listCountBefore + 1, listCountAfter);
+        }
+
+        [TestMethod]
+        public void deleteVehicle_Test()
+        {
+            Vehicle.DeleteVehicle(TestClass.v);
+        }
+
+        [TestMethod]
+        public void editVehicle_Test()
+        {
+            Vehicle.EditVehicle(TestClass.v.Id, "TestMake1", "testmodel1", 2011, "1DSA333", 1001, 45);
+
+            TestClass.v = Vehicle.vehicleList.Where(x => x.Manufacturer.Contains("TestMake")).FirstOrDefault();
+
+            Assert.AreEqual(TestClass.v.Manufacturer, "TestMake1");
+            Assert.AreEqual(TestClass.v.Model, "testmodel1");
+            Assert.AreEqual(TestClass.v.MakeYear, 2011);
+            Assert.AreEqual(TestClass.v.RegistrationNumber, "1DSA333");
+            Assert.AreEqual(TestClass.v.OdometerReading, 1001);
+            Assert.AreEqual(TestClass.v.TankCapacity, 45);
+        }
+
+        [TestMethod]
+        public void updateOdometer_Test()
+        {
+            string expectedResult = Vehicle.UpdateOdometer(TestClass.v.Id, 2000);
+
+            Assert.AreEqual(expectedResult, "");
+        }
+
+        [TestMethod]
+        public void updateOdometer_Test2()
+        {
+            string expectedResult = Vehicle.UpdateOdometer(TestClass.v.Id, 1);
+
+            Assert.AreEqual(expectedResult, "New Odometer is lower than actual");
+        }       
+    }
+}
